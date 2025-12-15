9d0402b9a6bcd688b088884e42ee4f4a0fb01a44
Ikhsan Alparizki <105845193+Lonelyman86@users.noreply.github.com>
2025-12-15 11:54:11 +0700

    Rename LikeController to NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,7 +9,7 @@ use App\Models\Notification; // <--- WAJIB: Import Model Notification
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LikeController extends Controller
+class NotificationController extends Controller
 {
     public function toggle($photoId)
     {
